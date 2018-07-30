2045f93eeb21edc35e8ec926d60a8616f414aef5
Lunatik(Ленар) <lunatik2000@inbox.ru>
2018-07-30 23:51:58 +0300

    Initial commit

---
+++ IpCountProg/IpAdress.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+namespace IpCountProg
+{
+    public class IpRepresentation : IPAddress
+    {
+
+        public IpRepresentation(byte[] ip) : base(ip)
+        {
+        }
+
+        public byte[] ToIpParts(int b)
+        {
+            return Parse(b.ToString()).GetAddressBytes();
+        }
+
+        public override string ToString()
+        {
+            var ipOctets = GetAddressBytes();
+            return $"{ipOctets[0]}.{ipOctets[1]}.{ipOctets[2]}.{ipOctets[3]}";
+        }
+
+        public virtual string BinaryView()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var addressByte in GetAddressBytes())
+            {
+                sb.Append(Convert.ToString(addressByte, 2).PadLeft(8, '0')+".");
+            }
+            sb.Remove(sb.Length - 1, 1);
+            return sb.ToString();
+        }
+    }
+
+    public class IpMask : IpRepresentation
+    {
+        public int MaskNumber { get; set; }
+        
+
+        public IpMask(byte[] ip) : base(ip)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                ip[i] = ReverseByte(ip[i]);
+            }
+            var num = CheckMask(new BitArray(ip));
+            if (num == -1)
+                throw new ArgumentException("Its not a  mask, you idiot");
+            MaskNumber = num;
+        }
+
+        public static int CheckMask(BitArray b)
+        {
+            var j = 0;
+            int i;
+            for (i = 0; i < 32; i++)
+            {
+                if (!b[i])
+                    break;
+                j++;
+            }
+
+            if (i == 31)
+                return j;
+
+            for (; i < 32; i++)
+            {
+                if (b[i])
+                    return -1;
+            }
+            return j;
+        }
+
+        protected byte ReverseByte(byte originalByte)
+        {
+            int result = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                result = result << 1;
+                result += originalByte & 1;
+                originalByte = (byte)(originalByte >> 1);
+            }
+
+            return (byte)result;
+        }
+
+        public override string ToString()
+        {
+            var ipOctets = GetAddressBytes();
+            return string.Format("({4})  {0}.{1}.{2}.{3}", ipOctets[0], ipOctets[1], ipOctets[2], ipOctets[3], MaskNumber);
+        }
+
+        public int GetNetworkCapacity()
+        {
+            var d = Math.Pow(2, (32 - MaskNumber));
+            return (int)d - 2;
+        }
+    }
+
+    public class MyIp : IpRepresentation
+    {
+
+        public MyIp(byte[] ip) : base(ip)
+        {
+        }
+
+        public IpRepresentation GetNetworkAdress(IpMask mask)
+        {
+            var network = new byte[4];
+            var ipOctets = GetAddressBytes();
+            var maskOctets = mask.GetAddressBytes();
+            for (var i = 0; i < 4; i++)
+            {
+                network[i] = (byte)(ipOctets[i] & maskOctets[i]);
+            }
+            return new IpRepresentation(network);
+        }
+
+        public IpRepresentation GetBroadcastAdress(IpMask mask)
+        {
+            var network = new byte[4];
+            var ipOctets = base.GetAddressBytes();
+            var maskOctets = mask.GetAddressBytes();
+            for (var i = 0; i < 4; i++)
+            {
+                network[i] = (byte)(ipOctets[i] | (255 - maskOctets[i]));
+            }
+            return new IpRepresentation(network);
+        }
+
+        public string GetIpClass()
+        {
+            var ipOctets = base.GetAddressBytes();
+            if ((ipOctets[0] & 128) == 0)
+                return "A";
+            if ((ipOctets[0] & 192) == 128)
+                return "B";
+            if ((ipOctets[0] & 224) == 192)
+                return "C";
+            if ((ipOctets[0] & 240) == 224)
+                return "D";
+            return "E";
+        }
+
+
+        public static IEnumerable<string> GetLocalIpAddress()
+        {
+            var host = Dns.GetHostAddresses(Dns.GetHostName());
+            return from ip in host
+                   where ip.AddressFamily == AddressFamily.InterNetwork
+                   select ip.ToString();
+        }
+
+        public static IEnumerable<PhysicalAddress> GetMacAddress()
+        {
+            return from nic in NetworkInterface.GetAllNetworkInterfaces()
+                   where nic.OperationalStatus == OperationalStatus.Up && nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet && nic.Name == "Ethernet"
+                   select nic.GetPhysicalAddress();
+        }
+
+        public static IpMask CountMaskFromNumber(string text)
+        {
+            if (text == string.Empty)
+                throw new ArgumentException("No emtpy strings");
+            var pcNumber = Convert.ToInt32(text);
+            var maskNumber = Math.Log(pcNumber + 2, 2);
+            if (Math.Floor(maskNumber) != maskNumber)
+                maskNumber++;
+            var count = 32 - (int)(maskNumber);
+            var bytes = new byte[4];
+            int i;
+            for (i = 0; i < (int)(count / 8); i++)
+            {
+                bytes[i] = 255;
+            }
+            byte result = 0;
+            for (int j = 0; j < count % 8; j++)
+            {
+                result += (byte)Math.Pow(2, (7 - j));
+            }
+            bytes[i] = result;
+            return new IpMask(bytes);
+        }
+    }
+}
---
+++ IpCountProg/MainWindow.xaml.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace IpCountProg
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    { 
+        private int _ipPosition;
+        private int _countPosition;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            byte mask = 255;
+            byte[] result = new byte[4];
+            for (var i = 3; i >= 0; i--)
+            {
+                result[i] = 255;
+            }
+            for (var j = 3; j >= 0; j--)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    mask = (byte)(mask << 1);
+                    result[j] = mask;
+                    var msk = new IpMask(result);
+                    MaskComboBox.Items.Add(msk);
+                }
+                mask = 255;
+            }
+            DisplayIpAdr();
+            DisplayMacAdr();
+        }
+
+        private void IpTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsOnlyNumbers(e.Text);
+            var ipTextBox = sender as TextBox;
+            if (ipTextBox != null)
+                _ipPosition = ipTextBox.CaretIndex;
+        }
+
+        private void IpTextBox_OnPaste(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                var text = (string)e.DataObject.GetData(typeof(string));
+                if (!IsOnlyNumbers(text))
+                {
+                    e.CancelCommand();
+                }
+                if (IsNotByte(text))
+                    e.CancelCommand();
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private static bool IsOnlyNumbers(string text)
+        {
+            var regex = new Regex("[^0-9]+");
+            return !regex.IsMatch(text);
+        }
+
+        private bool IsNotByte(string number)
+        {
+            try
+            {
+                var n = Convert.ToInt32(number);
+                return n > 255;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private void IpTextBox1_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var target = (TextBox)sender;
+            if (IsNotByte((target.Text)))
+            {
+                var text = target.Text;
+                target.Text = text.Remove(_ipPosition, 1);
+                target.CaretIndex = _ipPosition;
+            }
+        }
+
+        private void OkButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var mask = MaskComboBox.SelectedItem as IpMask;
+            if (mask != null && IpTextBox1.Text != string.Empty && IpTextBox2.Text != string.Empty
+                && IpTextBox3.Text != string.Empty && IpTextBox4.Text != string.Empty)
+            {
+                var ipAdress =
+                    new MyIp(new[]
+                    {
+                        Convert.ToByte(IpTextBox1.Text), Convert.ToByte(IpTextBox2.Text),
+                        Convert.ToByte(IpTextBox3.Text), Convert.ToByte(IpTextBox4.Text)
+                    });
+                DisplayData(ipAdress,mask);
+
+            }
+        }
+
+        private void DisplayData(MyIp ip,IpMask mask)
+        {
+            var brAdr = ip.GetBroadcastAdress(mask);
+            var netAdr = ip.GetNetworkAdress(mask);
+            var l = new ObservableCollection<Representation>
+            {
+                new Representation { Name = "Ip adress",BinaryValue = ip.BinaryView(), Value = ip.ToString()},
+                new Representation { Name = "Mask", BinaryValue = mask.BinaryView(),Value = mask.ToString()},
+                new Representation { Name = "Broadcast adress", BinaryValue = brAdr.BinaryView(),Value = brAdr.ToString()},
+                new Representation { Name = "Network adress", BinaryValue = netAdr.BinaryView(), Value = netAdr.ToString()},
+            };
+            IpRepresentationListView.ItemsSource = l;
+            IpClassTextBox.Text = ip.GetIpClass();
+            IpClassTextBox.IsEnabled = true;
+            CountTextBox.Text = mask.GetNetworkCapacity().ToString();
+            CountTextBox.IsEnabled = true;
+
+        }
+
+        private void DisplayIpAdr()
+        {
+            var adresses = MyIp.GetLocalIpAddress();
+            var adressesArray = adresses as string[] ?? adresses.ToArray();
+            if (adressesArray.Any())
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var adress in adressesArray)
+                {
+                    sb.Append(adress + Environment.NewLine);
+                }
+                ComputerIpTextBox.Text = sb.ToString().TrimEnd('\r', '\n');
+            }
+            else
+            {
+                ComputerIpTextBox.Text = "нет";
+            }
+        }
+
+        private void DisplayMacAdr()
+        {
+            var mac = MyIp.GetMacAddress();
+            var physicalAddresses = mac as PhysicalAddress[] ?? mac.ToArray();
+            if (physicalAddresses.Any())
+            {
+                var sb = new StringBuilder();
+                foreach (var adress in physicalAddresses)
+                {
+                    sb.Append(adress + Environment.NewLine);
+                }
+                ComputerMacTextBox.Text = sb.ToString().TrimEnd('\r', '\n');
+            }
+            else
+            {
+                ComputerMacTextBox.Text = "нет";
+            }
+        }
+
+        private void PcCountTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var target = (TextBox)sender;
+            if (target.Text == string.Empty)
+                return;
+            target.Text = target.Text.Replace("  ", string.Empty);
+            var n = Convert.ToInt64(target.Text);
+            if (n > (double)int.MaxValue + 2)
+            {
+                var text = target.Text;
+                target.Text = text.Remove(_countPosition, 1);
+                target.CaretIndex = _countPosition;
+            }
+
+        }
+
+        private void PcCountTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsOnlyNumbers(e.Text);
+            var ipTextBox = sender as TextBox;
+            if (ipTextBox != null)
+                _countPosition = ipTextBox.CaretIndex;
+        }
+
+        private void PcCountTextBox_OnPasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                var text = (string)e.DataObject.GetData(typeof(string));
+                if (text == string.Empty)
+                    return;
+                if (!IsOnlyNumbers(text))
+                {
+                    e.CancelCommand();
+                    return;
+                }
+                if (text != null)
+                {
+                    text = text.Replace("  ", string.Empty);
+                    long n = Convert.ToInt64(text);
+                    if (n > (double)int.MaxValue + 2)
+                        e.CancelCommand();
+                }
+                else
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private void CountButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var text = PcCountTextBox.Text;
+            var mask = MyIp.CountMaskFromNumber(text);
+            CountMaskTextBox.Text = "Маска: " + mask;
+        }
+    }
+    public class Representation
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string BinaryValue { get; set; }
+    }
+}
